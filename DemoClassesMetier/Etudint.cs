ff43b5d7ae27d97b35d87af07b58a799da8ae046
jbuffeteau <jacquesbuffeteau@yahoo.fr>
2021-11-11 16:03:43 +0100

    création de la classe etudiant

---
+++ DemoClassesMetier/Etudint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoClassesMetier
+{
+    class Etudint
+    {
+        // test
+        private int id;
+    }
+}
